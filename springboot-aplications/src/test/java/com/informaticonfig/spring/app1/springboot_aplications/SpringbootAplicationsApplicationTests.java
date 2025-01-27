58f9d5c511e94a7fe16c0e3e536323ec42edfa6e
brunoalbin23 <albinbruno23@gmail.com>
2025-01-27 00:01:41 -0300

    update - 10%

---
+++ springboot-aplications/src/test/java/com/informaticonfig/spring/app1/springboot_aplications/SpringbootAplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informaticonfig.spring.app1.springboot_aplications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAplicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
